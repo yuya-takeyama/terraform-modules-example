a50c44a4a2fbfb12561b44d6c41faf8c8d7bedc2
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-08-01 01:02:38 +0900

    Update module

--- modules/foo/variables.tf
+++ modules/foo/variables.tf
@@ -1,3 +1,8 @@
+variable "count" {
+  type        = number
+  description = "Number of null resources"
+}
+
 variable "triggers" {
   type        = map(string)
   description = "Change this value to trigger re-provisioning"
